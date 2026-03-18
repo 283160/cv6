8282276782f73d1c582f9dd618d1ae6679fe9ea3
Jitka Dvořáková <283160@vutbr.cz>
2026-03-18 10:39:59 +0100

    pokus

---
+++ src/circles_project/circles_intersection.py
@@ -0,0 +1,11 @@
+circle_1 = {"x": 0, "y": 0, "r": 2}
+circle_2 = {"x": 3, "y": 0, "r": 1}
+
+from circles_stats import has_intersection
+prunik = has_intersection(circle_1, circle_2)
+if prunik["intersects"] == True and prunik["intersections_count"] == 2:
+    print("Kružnice mají dva průniky.")
+elif prunik["intersects"] == True and ["intersections_count"] == 1:
+    print("Kružnice se dotýkají v jednom bodě.")
+else:
+    print("Kružnice se nedotýkají.")
